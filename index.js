5c404bc2948e9885272175a376811e1555f62782
Ben Smerd <bsmerd@adam.com.au>
2022-11-21 09:29:12 +1030

    init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// * running application where import strava data on runs
+
+//* have user login and have friends
+
+//* create specific groups with people
+
+//* strava API gets the users logged runs everyday and uploads the data to the page
+
+// * can have categories- most km run this week/month/year, quickest run, furthest run etc
+
+//* goup can create a betting system and it automaticall calculate the winner by calculating that specific category
+
+//* if wanted to take step further, add in crypto wallet and smart contracts so users can actually put friendly wages on and automatically gets transacted
+
+// * typescript/react/JWT/axios/
+
+//* database use MongoDB because not relational, just adding running data and then comparing to others
+
+//* Mongoose as the ODM or try find another good NOSQL database/ODM
+
+//* just use express as not much info to get
