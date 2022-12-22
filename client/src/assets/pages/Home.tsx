7a47c4270a108784b864cc3a79b449e7458e1aed
Ben Smerd <bsmerd@adam.com.au>
2022-12-22 17:06:41 +1030

    updated styling for the group leaderboard container

--- client/src/assets/pages/Home.tsx
+++ client/src/assets/pages/Home.tsx
@@ -3,7 +3,7 @@ import SoloStats from "../components/feature/homePage/SoloStats";
 
 const Home = () => {
   return (
-    <div className="flex flex-col p-2 h-full gap-7">
+    <div className="flex flex-col p-2 h-full gap-4">
       <p className="text-sm">
         Create an account and then you will be able to join a friend group. Once
         you have joined a group with your friends you will be able to compare
