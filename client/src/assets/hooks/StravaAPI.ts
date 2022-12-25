2835130135a2c66211205a91938b17322d186b0e
Ben Smerd <bsmerd@adam.com.au>
2022-12-25 12:55:01 +1030

    update Strava api fetch call

--- client/src/assets/hooks/StravaAPI.ts
+++ client/src/assets/hooks/StravaAPI.ts
@@ -1,3 +1,35 @@
+import axios from "axios";
+
 // * strava API call
 
 // https://developers.strava.com/docs/reference/
+
+// https://www.strava.com/api/v3/athlete/activities?access_token=d80607ab2c531dd251bb4a79b624a99927927de1
+
+const authLink = "https://www.strava.com/oauth/token";
+
+export const stravaAPI = async () => {
+  try {
+    const response = await axios.get(
+      "https://www.strava.com/api/v3/athlete?access_token=d80607ab2c531dd251bb4a79b624a99927927de1"
+    );
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// const reAuth = async () => {
+//   const options = {
+//     url: authLink,
+//     method: "POST",
+//     headers: {
+//       Accept: "application/json, text/plain, */*",
+//     },
+//   };
+//   try {
+//     const response = axios.post(options);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
