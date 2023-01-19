bf27f79fb6906f24fbf80531598b3e4b654ec2a0
Ben Smerd <bsmerd@adam.com.au>
2023-01-19 17:45:52 +1030

    added in the strava function to get access code and authorization code

--- client/src/assets/hooks/StravaAPI.ts
+++ client/src/assets/hooks/StravaAPI.ts
@@ -11,7 +11,7 @@ const clientID = "99017";
 const myId = "55591743";
 const clientSecret = "683f17caa4792c34914e0ce7af1217469ced16bd";
 const refresh = "a855f824883392d03e6ce3cf6826bf069dd8be93";
-const accessToken = "bb68befcce2814559ad6a09478caa40b5f258dcc";
+const accessToken = "dd5d8402dab0d18bb320235187b7fe441e840fa8";
 
 //* 1- after user authrizes they get autorization code in url--
 //TODO need to create fucton to get that
@@ -21,10 +21,11 @@ const OAuthLink = `http://www.strava.com/oauth/authorize?client_id=99017&respons
 //* OR 1--- need to find out if does redirect properly back to runnenrs after authorze
 //? This redirects to runners with the approval token in the URL-- THIS DOESNT GO IN A FETHC CALL THE USER LITERALLY NEEDS TO BE REACT-LINK SENT TO IT AND THEN CLICK AUTHORIZE WILL THEN SEND THEM TO THE APP WITH THE CODE IN THE URL !!!!
 //!!!!! ABOVE
-const OAuthLinkTest = `http://www.strava.com/oauth/authorize?client_id=99017&response_type=code&redirect_uri=http://localhost:5173/exchange_token&approval_prompt=force&scope=activity:read_all`;
+const OAuthLinkTest = `http://www.strava.com/oauth/authorize?client_id=99017&response_type=code&redirect_uri=http://localhost:5173/connect/exchange_token&approval_prompt=force&scope=activity:read_all`;
 
 //* 2-- need to create function that then calls this with the authorization code from num 1 and then this returns the athletes info like id etc, and an access token which is used for accessing their data
 //TODO need to make a refresher token function to continiously get their access token for data uploading
+//TODO Will pobably need to useParams to get the url link and then break it down to et the code
 const authLink = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&code=1d0c6b28399a17b1e09d80651ed35355f2147bc3&grant_type=authorization_code`;
 
 //? My strava data with accessToken
@@ -50,6 +51,41 @@ export const stravaAPI = async () => {
     console.log(response);
   } catch (error) {
     console.log(error);
+  }
+};
+
+//? Function that gets the auth code to then get the access code
+export const stravaAuthToken = async (code: string) => {
+  try {
+    const { data } = await axios.post("https://www.strava.com/oauth/token", {
+      client_id: clientID,
+      client_secret: clientSecret,
+      code,
+    });
+    console.log(data);
+    const accessToken = data.access_token;
+    getActivities(accessToken);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//? Function that gets users activities
+//TODO need to make this update regularly with the epoch timestap for day before-- COULD GET THE DATE THE USER JOINED AND THEN
+//TODO need to somehow make it scan through the different pages as users can have more than 200-- just need to have a counter for the page={counter} and increment it
+const getActivities = async (accessToken: string) => {
+  try {
+    const { data } = await axios.get(
+      `https://www.strava.com/api/v3/athlete/activities?before=1671954505&after=1232349016&page=1&per_page=200`,
+      {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    console.log(data);
+  } catch (err) {
+    console.log(err);
   }
 };
 
